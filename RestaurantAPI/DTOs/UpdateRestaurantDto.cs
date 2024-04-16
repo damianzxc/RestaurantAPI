dde5a0c9e7093c82f0a2081347da8cc1b277bc11
Damian Kaluzny <kaluznydamian@gmail.com>
2024-04-16 18:53:07 +0200

    Forgotten UpdateRestaurantDTO Validation has been added

--- RestaurantAPI/DTOs/UpdateRestaurantDto.cs
+++ RestaurantAPI/DTOs/UpdateRestaurantDto.cs
@@ -1,7 +1,11 @@
-﻿namespace RestaurantAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI.DTOs
 {
     public class UpdateRestaurantDto
     {
+        [Required]
+        [MaxLength(25)]
         public string? Name { get; set; }
         public string? Description { get; set; }
         public bool HasDelivery { get; set; }
